747b5f7624d25722c9056332a92828288526ff5e
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-30 15:55:08 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/capgemini/workshopmongo/config/Instantiation.java
+++ src/main/java/com/capgemini/workshopmongo/config/Instantiation.java
@@ -1,25 +1,40 @@
 package com.capgemini.workshopmongo.config;
 
+import com.capgemini.workshopmongo.domain.Post;
 import com.capgemini.workshopmongo.domain.User;
+import com.capgemini.workshopmongo.repository.PostRepository;
 import com.capgemini.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat simpleDF = new SimpleDateFormat("dd/MM/yyyy");
+        simpleDF.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, simpleDF.parse("09/02/2024"), "Partiu viajem", "Vou viajar pra caragua.", bob);
+        Post post2 = new Post(null, simpleDF.parse("01/03/2024"), "Paraquedismo", "Dia de pular de paraquedas.", alex);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/capgemini/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.capgemini.workshopmongo.repository;
+
+import com.capgemini.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
