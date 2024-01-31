5d3dac0afb9990481d972c620bf75a241a191452
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-31 10:43:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/capgemini/workshopmongo/config/Instantiation.java
+++ src/main/java/com/capgemini/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.capgemini.workshopmongo.config;
 import com.capgemini.workshopmongo.domain.Post;
 import com.capgemini.workshopmongo.domain.User;
 import com.capgemini.workshopmongo.dto.AuthorDTO;
+import com.capgemini.workshopmongo.dto.CommentDTO;
 import com.capgemini.workshopmongo.repository.PostRepository;
 import com.capgemini.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.TimeZone;
 
 @Configuration
@@ -36,6 +38,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, simpleDF.parse("09/02/2024"), "Partiu viajem", "Vou viajar pra caragua.", new AuthorDTO(bob));
         Post post2 = new Post(null, simpleDF.parse("01/03/2024"), "Paraquedismo", "Dia de pular de paraquedas.", new AuthorDTO(bob));
+
+        CommentDTO comment1 = new CommentDTO("Boa viajem mano.", simpleDF.parse("09/02/2024"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite", simpleDF.parse("09/02/2024"), new AuthorDTO(maria));
+        CommentDTO comment3 = new CommentDTO("Tenha um otimo salto.", simpleDF.parse("01/03/2024"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
